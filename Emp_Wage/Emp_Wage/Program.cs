1b1db47cfc34647d77851918bd3b6757444aebea
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-10-31 22:44:12 +0530

    uc2_Emp_fullday

--- Emp_Wage/Emp_Wage/Program.cs
+++ Emp_Wage/Emp_Wage/Program.cs
@@ -5,20 +5,35 @@ namespace Emp_wage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CALCULATING DAILY ATTENDENCE");
+             Console.WriteLine("CALCULATING DAILY WAGES");
             int IS_PRESENT = 1;
+            int isFullTime = 1;
+            int empHr = 0;
+            int empRatePerHr = 20;
             Random random = new Random();
             //computation
             int empCheck = random.Next(0, 2);
+
             if (empCheck == IS_PRESENT)
             {
-                Console.WriteLine("Employes is Present");
+                Console.WriteLine("Employe is Present");
+                int empCheckFull = random.Next(0, 2);
+                if (empCheckFull == isFullTime)
+                {
+                    Console.WriteLine("present for fullday");
+                    empHr = 8;
+                }
+                else
+                {
+                    Console.WriteLine("Present for half day");
+                }
             }
             else
             {
                 Console.WriteLine("Employe is Absent");
-                Console.ReadLine();
             }
+            int salary = (empHr * empRatePerHr);
+            Console.WriteLine("Employe salary :" + salary);
         }
         
     }
