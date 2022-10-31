7b3eae472a26c707f3fbe4292de9498502616e63
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-10-31 22:09:32 +0530

    uc1

---
+++ Emp_Wage/Emp_Wage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Emp_wage
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATING DAILY ATTENDENCE");
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employes is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employe is Absent");
+            }
+        }
+    }
+}
